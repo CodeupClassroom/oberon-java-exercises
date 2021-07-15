ff25497763ec4f9a59116ce6136426f5b38f342c
vegetasrevenge <cjedwards233@yahoo.com>
2021-07-15 15:54:47 -0500

    adds I/O material

---
+++ src/fileIO/FileIO.java
@@ -0,0 +1,32 @@
+package fileIO;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileIO {
+
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static void init(){
+
+        // Try to create the directory
+        Path path = FileDirectoryUtil.getPath("src", "fileIO", "files");
+        FileDirectoryUtil.tryCreateDirectory(path);
+
+        // Try to create the file
+        path = Paths.get(path.toString(), "test.txt");
+        FileDirectoryUtil.tryCreateFile(path);
+
+        // Print out the final location of the file
+        System.out.println(path.toAbsolutePath());
+        IOUtil.tryPrintContents(path);
+
+    }
+
+
+
+
+}
